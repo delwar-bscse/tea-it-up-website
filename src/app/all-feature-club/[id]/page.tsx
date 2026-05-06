27d4ef5e2f850f6c11e4c5197e9c2b70349cc759
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-05-06 11:03:56 +0600

    details

--- src/app/all-feature-club/[id]/page.tsx
+++ src/app/all-feature-club/[id]/page.tsx
@@ -1,11 +1,13 @@
 import Architecture from "./Architecture"
 import ClubDetailsHero from "./ClubDetailsHero"
+import TeaTimes from "./TeaTimes"
 
 const ClubDetails = () => {
   return (
     <div>
       <ClubDetailsHero />
       <Architecture />
+      <TeaTimes />
     </div>
   )
 }
